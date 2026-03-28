835118547d8d9c639c7bbd63d8f9eaaeefb18567
Wilson <wilsonyu657@gmail.com>
2026-03-28 14:50:33 -0400

    Add requestIdleCallBack rollback in case unsupported

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/global.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
 
+if (!window.requestIdleCallback || !window.cancelIdleCallback) {
+  console.warn(
+    "requestIdleCallback/cancelIdleCallback are not supported by this browser.",
+  );
 
-const faLink = document.createElement('link');
-faLink.rel = 'stylesheet';
-faLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css';
-faLink.media = 'print'; 
-faLink.onload = function() { (this as HTMLLinkElement).media = 'all'; };
+  window.requestIdleCallback = (callback: IdleRequestCallback) => {
+    return window.setTimeout(
+      () => callback({ didTimeout: true, timeRemaining: () => 0 }),
+      0,
+    );
+  };
+
+  window.cancelIdleCallback = (id: number) => {
+    window.clearTimeout(id);
+  };
+}
+
+const faLink = document.createElement("link");
+faLink.rel = "stylesheet";
+faLink.href =
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css";
+faLink.media = "print";
+faLink.onload = function () {
+  (this as HTMLLinkElement).media = "all";
+};
 document.head.appendChild(faLink);
- 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
